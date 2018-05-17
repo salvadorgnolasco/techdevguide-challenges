34a15529edf67f39f130560f36b199c70216f7ff
Salvador Gonzalez Nolasco <salvador.gonzalez@softtek.com>
2018-05-17 14:07:41 -0500

    new header

--- src/test/java/foundation/longestword/LongestWordSeekerTest.java
+++ src/test/java/foundation/longestword/LongestWordSeekerTest.java
@@ -1,13 +1,19 @@
 /* ----------------------------------------------------------------------------
- * All rights reserved © 2018 Softtek.
+ * All rights reserved © 2018 Salvador Gonzalez N.
  *  
- * This software contains information that is exclusive property of Softtek,
- * this information is considered confidential.
- * It is strictly forbidden the copy or spreading of any part of this document 
- * in any format, whether mechanic or electronic.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------------
  * File name: LongestWordSeekerTest.java
- * Original Author: Softtek
+ * Original Author: Salvador Gonzalez N.
  * Creation Date: 17/05/2018
  * ---------------------------------------------------------------------------
  */
